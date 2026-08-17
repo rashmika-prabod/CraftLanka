7e4b53c42017fef3ed1311dcb3ccbc5490d66419
seshan <seshanprof@gmail.com>
2026-08-17 11:47:08 +0530

    Make gradlew executable

---
+++ app/src/main/java/com/craftlanka/app/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.craftlanka.app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState: Bundle?)
+        setContentView(R.layout.activity_main)
+    }
+}
